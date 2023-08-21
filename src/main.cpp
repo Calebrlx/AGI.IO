297f4859dcd533d45090e6bc841cfae6d5212f8a
Calebslc <133164966+Calebslc@users.noreply.github.com>
2023-08-21 04:02:16 -0800

    a

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,21 @@
 #include <iostream>
 
 int main() {
-  Preprocessor io;
-  std::string processed_text = io.promptInput();
-  std::cout << "Processed text: " << processed_text << std::endl;
+  Preprocessor preprocessor;
+  std::cout << "Type something: ";
+  std::string userInput;
+  std::getline(std::cin, userInput);
+
+  // Apply preprocessing steps
+  std::vector<std::string> tokens = preprocessor.tokenize(userInput);
+  std::string lowercaseText = preprocessor.toLowerCase(userInput);
+  std::string noPunctuationText = preprocessor.removePunctuation(lowercaseText);
+  std::vector<std::string> noStopwordsTokens = preprocessor.removeStopwords(tokens);
+  std::vector<std::string> stemmedTokens = preprocessor.stem(noStopwordsTokens);
+  std::vector<std::string> lemmatizedTokens = preprocessor.lemmatize(stemmedTokens);
+
+  // Print processed text (you may want to concatenate the lemmatizedTokens into a single string)
+  std::cout << "Processed text: " << noPunctuationText << std::endl;
+
   return 0;
 }
