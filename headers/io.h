e6f3927e4d5a71a10d1dcb74cf53d560d8606cbf
Calebslc <133164966+Calebslc@users.noreply.github.com>
2023-08-21 02:31:58 -0800

    g

--- headers/io.h
+++ headers/io.h
@@ -1,6 +1,17 @@
 #ifndef IO_H
 #define IO_H
 #include <string>
+#include <fst/fstlib.h>
+
+class TextPreprocessor {
+public:
+  TextPreprocessor();
+  std::string preprocess(const std::string& input_text);
+
+private:
+  fst::StdVectorFst fst_;
+  void initFST();
+};
 
 class IO {
 public:
