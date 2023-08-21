9484ceb71a38bd4727d975075594bc3280e55eb1
Calebslc <133164966+Calebslc@users.noreply.github.com>
2023-08-20 21:38:05 -0800

    Update io.cpp

--- src/io.cpp
+++ src/io.cpp
@@ -1,1 +1,17 @@
+#include "io.h"
+#include <iostream>
 
+IO::IO() {
+    // Constructor implementation (if needed)
+}
+
+IO::~IO() {
+    // Destructor implementation (if needed)
+}
+
+std::string IO::promptInput() {
+    std::cout << "Type something: ";
+    std::string userInput;
+    std::getline(std::cin, userInput);
+    return userInput;
+}
