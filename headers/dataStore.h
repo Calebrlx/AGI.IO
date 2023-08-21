b0f6a826b30bf4aec76fbe48ea80d751c1b87c48
Calebslc <133164966+Calebslc@users.noreply.github.com>
2023-08-20 21:09:25 -0800

    Update dataStore.h

--- headers/dataStore.h
+++ headers/dataStore.h
@@ -1,1 +1,15 @@
+#ifndef DATA_STORE_H
+#define DATA_STORE_H
 
+#include <string>
+#include <vector>
+
+class DataStore {
+public:
+    DataStore();
+    ~DataStore();
+    void addData(const std::string &data);
+    std::vector<std::string> getAllData() const;
+};
+
+#endif // DATA_STORE_H
