8060f7d4a7e168ae59cb88fef45bed8b8dc3e79d
Calebslc <133164966+Calebslc@users.noreply.github.com>
2023-08-20 21:57:53 -0800

    Update cacheManager.h

--- headers/cacheManager.h
+++ headers/cacheManager.h
@@ -1,1 +1,22 @@
+#ifndef CACHE_MANAGER_H
+#define CACHE_MANAGER_H
 
+#include "dataStore.h"
+#include "nlp.h"
+#include <string>
+#include <unordered_map>
+
+class CacheManager {
+public:
+    CacheManager(DataStore* dataStore, NLP* nlp);
+    std::string processInput(const std::string &input);
+    void updateCache(const std::string &key, const std::string &value);
+    std::string retrieveFromCache(const std::string &key);
+
+private:
+    DataStore* dataStore;
+    NLP* nlp;
+    std::unordered_map<std::string, std::string> cache;
+};
+
+#endif // CACHE_MANAGER_H
