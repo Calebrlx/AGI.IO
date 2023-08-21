ee7a380c400d47a67db506a13fc626ff2363bed6
Calebslc <133164966+Calebslc@users.noreply.github.com>
2023-08-21 03:44:01 -0800

    git

--- src/io.cpp
+++ src/io.cpp
@@ -1,77 +1,84 @@
-#include "../headers/io.h"
-#include <iostream>
-#include <sstream>
+#include "Preprocessor.h"
 #include <cctype>
+#include <sstream>
+#include <algorithm>
+#include <vector>
+#include <unordered_set>
 
-TextPreprocessor::TextPreprocessor() {
-    // Initialize FST
-    initFST();
+
+Preprocessor::Preprocessor() {
+    // Initialize any resources needed for preprocessing
+    // No specific initialization required for this example
 }
 
-std::string TextPreprocessor::preprocess(const std::string& input_text) {
-    std::string result;
-    fst::StdVectorFst output;
-
-    // Start from the initial state
-    int current_state = fst_.Start();
-
-    // Iterate through the input text
-    for (char c : input_text) {
-        if (std::isalnum(c) || c == ' ') {
-            // Follow the transition for the current character
-            fst::ArcIterator<fst::StdVectorFst> aiter(fst_, current_state);
-            for (; !aiter.Done(); aiter.Next()) {
-                const fst::StdArc &arc = aiter.Value();
-                if (arc.ilabel == std::tolower(c)) {
-                    result.push_back(arc.olabel);
-                    current_state = arc.nextstate;
-                    break;
-                }
-            }
-        }
+std::vector<std::string> Preprocessor::tokenize(const std::string& text) {
+    // Break the input text into smaller units (words or subwords)
+    std::vector<std::string> tokens;
+    std::istringstream iss(text);
+    std::string token;
+    while (iss >> token) {
+        tokens.push_back(token);
     }
+    return tokens;
+}
 
+std::string Preprocessor::toLowerCase(const std::string& text) {
+    // Convert all characters in the text to lowercase
+    std::string result = text;
+    for (char& c : result) {
+        c = std::tolower(c);
+    }
     return result;
 }
 
-void TextPreprocessor::initFST() {
-    int start = fst_.AddState();
-    fst_.SetStart(start);
-
-    // Add transitions for lowercase characters and space
-    int current_state = start;
-    for (char c = 'a'; c <= 'z'; c++) {
-        int next_state = fst_.AddState();
-        fst_.AddArc(current_state, fst::StdArc(c, c, 0, next_state));
-        current_state = next_state;
+std::string Preprocessor::removePunctuation(const std::string& text) {
+    // Remove punctuation and special characters
+    std::string result;
+    for (char c : text) {
+        if (!std::ispunct(c)) {
+            result.push_back(c);
+        }
     }
-    for (char c = 'A'; c <= 'Z'; c++) {
-        int next_state = fst_.AddState();
-        fst_.AddArc(current_state, fst::StdArc(c, std::tolower(c), 0, next_state));
-        current_state = next_state;
-    }
-    // Add transition for space (tokenization)
-    int space_state = fst_.AddState();
-    fst_.AddArc(current_state, fst::StdArc(' ', ' ', 0, space_state));
-    fst_.SetFinal(space_state, 0);
+    return result;
 }
 
-IO::IO() {
-    // Constructor implementation (if needed)
+std::vector<std::string> Preprocessor::removeStopwords(const std::vector<std::string>& tokens) {
+    // Remove common stopwords
+    // Define a set of common stopwords (you can customize this list)
+    static const std::unordered_set<std::string> stopwords = {
+        "the", "is", "and", "of", "to", "in", "that", "it", "with", "as", "for", "on", "was", "at", "by", "an", "be", "this"
+        // Add more stopwords as needed
+    };
+
+    std::vector<std::string> result;
+    std::copy_if(tokens.begin(), tokens.end(), std::back_inserter(result),
+                 [&](const std::string& token) { return stopwords.find(token) == stopwords.end(); });
+    return result;
 }
 
-IO::~IO() {
-    // Destructor implementation (if needed)
+std::vector<std::string> Preprocessor::stem(const std::vector<std::string>& tokens) {
+    // Apply stemming to reduce words to their root form
+    // Stemming usually requires language-specific rules or external libraries (e.g., Snowball, Porter stemmer)
+    // For simplicity, this example will leave the tokens unchanged
+    // You may replace this with a real stemming algorithm if needed
+    return tokens;
 }
 
-std::string IO::promptInput() {
-    std::cout << "Type something: ";
-    std::string userInput;
-    std::getline(std::cin, userInput);
+std::vector<std::string> Preprocessor::lemmatize(const std::vector<std::string>& tokens) {
+    // Apply lemmatization to reduce words to their base or root form
+    // Lemmatization typically requires external libraries or tools (e.g., WordNet, SpaCy)
+    // For simplicity, this example will leave the tokens unchanged
+    // You may replace this with a real lemmatization algorithm if needed
+    return tokens;
+}
+std::vector<std::vector<float>> Preprocessor::wordEmbeddings(const std::vector<std::string>& tokens) {
+    // Convert words to vectors using pre-trained embeddings
+}
 
-    // Create TextPreprocessor object
-    TextPreprocessor preprocessor;
+std::vector<std::vector<float>> Preprocessor::padSequences(const std::vector<std::vector<float>>& sequences, int length) {
+    // Pad sequences to the same length
+}
 
-    // Preprocess the user input
-    return preprocessor.preprocess(userInput);
+std::vector<std::vector<float>> Preprocessor::preprocess(const std::string& text) {
+    // Apply all enabled preprocessing steps to the input text
 }
