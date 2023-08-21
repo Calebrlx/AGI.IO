6773c423170d9a3160f760c4369092ea6d1f4399
Calebslc <133164966+Calebslc@users.noreply.github.com>
2023-08-20 21:09:46 -0800

    Update dataStore.cpp

--- src/dataStore.cpp
+++ src/dataStore.cpp
@@ -1,1 +1,20 @@
+#include "dataStore.h"
 
+DataStore::DataStore() {
+    // Constructor implementation (if needed)
+}
+
+DataStore::~DataStore() {
+    // Destructor implementation (if needed)
+}
+
+void DataStore::addData(const std::string &data) {
+    store.push_back(data); // Adding data to the vector
+}
+
+std::vector<std::string> DataStore::getAllData() const {
+    return store; // Returning all stored data
+}
+
+// Private member variable to store the data
+std::vector<std::string> DataStore::store;
